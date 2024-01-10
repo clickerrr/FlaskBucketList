5ece41a7ad70e7677735696f99339c620761ae9b
clickerrr <bartekswiech1103@gmail.com>
2024-01-10 10:47:33 -0700

    Add dynamic .env debug mode

--- flaskblog/run.py
+++ flaskblog/run.py
@@ -1,5 +1,6 @@
 # this imports the app from the init file and then allows for the app to be ran with all the routes and stuff done
 from flaskblog import app
+from os import getenv
 # run
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=getenv("debug-mode") == "True")
